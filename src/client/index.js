3e2961a7b958454936f388184fe46d42d7db3538
Nahla531 <nhlaalekesh@gmail.com>
2021-01-14 16:52:01 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import './styles/resets.css'
+import './styles/header.css'
+import './styles/footer.css'
+import './styles/form.css'
+import './styles/base.css'
+import './styles/messages.css'
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
