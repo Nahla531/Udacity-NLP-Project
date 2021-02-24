b949c906b7892b96730fe4be2fe9ff9ee8c381bc
Nahla531 <nhlaalekesh@gmail.com>
2021-02-24 19:11:31 +0200

    finally-worked

--- src/client/js/textChecker.js
+++ src/client/js/textChecker.js
@@ -1,7 +1,6 @@
 function checkForText(inputText) {
     var regex = inputText.match(/^[a-zA-Z]+$/);
     if (regex == null) {
-        alert("Must input string");
         return 0;
     } else {
         return 1;
--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,6 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const bodyParser = require('body-parser')
-const superagent = require('superagent')
 const fetch = require('node-fetch')
 const dotenv = require('dotenv');
 // const { url } = require('inspector')
