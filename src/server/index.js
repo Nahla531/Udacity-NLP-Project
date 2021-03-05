4e1c79cc374cdf18bcf80bce5f14e89b7fcbf473
Nahla531 <nhlaalekesh@gmail.com>
2021-03-05 17:54:20 +0200

    deleted unwanted files

--- src/server/index.js
+++ src/server/index.js
@@ -1,72 +1,65 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const dotenv = require('dotenv');
+// const { url } = require('inspector')
+const cors = require('cors')
+const { response } = require('express')
+dotenv.config();
+console.log(`Your API key is ${process.env.API_KEY}`);
 
+//api vars
+const apiKey = process.env.API_KEY
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?"
 const app = express()
-
-<<<<<<< HEAD
-=======
 app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
->>>>>>> 67237c87e611ab0c921ef088b45e57676e389219
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function() {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res) {
     res.send(mockAPIResponse)
 })
-<<<<<<< HEAD
-=======
-const fetch = require("node-fetch");
-app.post('/data', function(req, res) {
-    // console.log(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&of=json&url=${req.body.input}`)
-    // fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&of=json&url=${req.body.input}`)
-    //     .then(data => data.json())
-    //     .then(res.send(data))
-    console.log(req.body)
-    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&of=json&url=${req.body.url}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data)
-            res.send({
-                score_tag: data.score_tag,
-                agreement: data.agreement,
-                subjectivity: data.subjectivity,
-                confidence: data.confidence,
-                irony: data.irony
-            })
 
 
+//api call
+//this whole part is from moilla network it helped me to do it 
+// here is the link :https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+app.post('/data-analayze', function(req, res) {
+
+    let text = req.body.text;
+    let url = `${baseUrl}key=${apiKey}&txt=${text}&lang=en`
+
+    fetch(url, {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        return response.json();
+    }).then((data) => {
+        console.log('data are', data)
+        res.send({
+            subjectivity: data.subjectivity,
+            agreement: data.agreement,
+            irony: data.irony,
+            score_tage: data.score_tag
         });
+    })
 
 })
-
-// app.post('/data', function(req, res) {
-//     const data = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&of=json&url=${req.body.url}`);
-//     try {
-//         // Transform into JSON
-//         const MeaningData = await data.json();
-//         response.send(MeaningData)
-//     } catch (error) {
-//         console.log("error", error);
-//         // appropriately handle the error
-//     }
-// })
->>>>>>> 67237c87e611ab0c921ef088b45e57676e389219
